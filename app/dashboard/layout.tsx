012b49209a8f37b9c5b2510696cd282606ab4e19
Duc Truong <dhtruong0407@gmail.com>
2025-12-09 22:46:07 +0700

    updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <div className="min-h-screen bg-sea-blue">
       <DashboardHeader />
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         {children}
       </main>
     </div>
